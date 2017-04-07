97a5657b336afb8ec562878c8aafba28096dcf82
David Gomez <david.gomez@i2cat.net>
2017-04-07 09:47:31 +0200

    Parse resolution anc convert in Int

--- main.c
+++ main.c
@@ -4,38 +4,30 @@
 
 void quaility_hander (char *h_device, char *w_device, char *qualities_mpd){
 
-//	char h_device = "800";
-//	char w_device = "600";
-//	char qualities_mpd = "480x240_3.23,600x300_5.22,1920x1080_5.22,2048x1024_5.22";
-	printf("-----\n");
-	printf( "%s\n", h_device );
-	printf( "%s\n", w_device );
-	printf( "%s\n", qualities_mpd );
-//
+
 	char buf[100]="";
-//	char temp[100]="";
 	int i = 0;
 	char *qualities[10];
 	char *resolution_to_check[2];
 	char *bitrate_to_check[0];
-	int number_qualities = 0;
 	char *token_w,*token_h;
 	int  with,height;
-//
-//	printf("--asi---\n");
-//
-//	qualities = qualities_mpd;
-//	printf( "%s\n", qualities );
-//
+	//Loop variables
+	int number_qualities = 0;
+	int index_to_check = 0;
+	//Print inputs
+	printf("-----\n");
+	printf( "%s\n", h_device );
+	printf( "%s\n", w_device );
+	printf( "%s\n", qualities_mpd );
+
+	//Copy quality to temp char
 	memset(buf, '\0', sizeof(buf));
-//	strcpy(temp, qualities);
 	strcpy(buf, qualities_mpd);
-//
 	char *p = strtok (buf, ",");
 	printf( "%s\n", p );
 
-//
-
+	//Parse qualities and obtain number of this
 	while (p != NULL)
 	{
 		printf( "%s\n", p );
@@ -46,30 +38,26 @@ void quaility_hander (char *h_device, char *w_device, char *qualities_mpd){
 
 	}
 	printf ("Number of qualities %d\n ", number_qualities);
-//	printf("Print all qualities:\n");
+
+	//For debug
+	printf("Print all qualities:\n");
 	printf("-----\n");
 	for (i = 0; i < number_qualities; ++i)
 		printf("%s\n", qualities[i]);
 	printf("-----\n");
-//
-	resolution_to_check[0] = strtok (qualities[0], "_");
-//
-	printf("%s\n", resolution_to_check[0]);
 
-	/* get the first token */
-	token_w = strtok(resolution_to_check[0], "x");
-	token_h = strtok(NULL, "x");
 
-	printf("--Char--\n");
-	printf( "%s\n", token_w );
-	printf( "%s\n", token_h );
+	for (i = 0; i < number_qualities; ++i){
+		resolution_to_check[i] = strtok (qualities[i], "_");
+		token_w = strtok(resolution_to_check[i], "x");
+		token_h = strtok(NULL, "x");
+		with = atoi(token_w);
+		height =atoi(token_h);
 
-	with = atoi(token_w);
-	height =atoi(token_h);
-
-	printf("--Int--\n");
-	printf( "%d\n", with );
-	printf( "%d\n", height );
+		printf("--Int--\n");
+		printf( "%d\n", with );
+		printf( "%d\n", height );
+	}
 
 }
 
